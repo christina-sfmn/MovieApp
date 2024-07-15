d3f7a771a4b2bd7ad399afb52457a0aeb3ede476
Christina Schöffmann <christina.schoeffmann@edu.fh-kaernten.ac.at>
2024-07-15 11:36:15 +0200

    Add image attribution

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,8 @@ export const Footer = () => {
       <p className="md:text-sm text-xs md:w-1/2 w-full">
         Created with OMDb API. The OMDb API is a RESTful web service to obtain movie
         information. All content and images on the site are contributed and
-        maintained by their users. More infos: <a href="https://www.omdbapi.com/" target="_blank" className="footer-link">www.omdbapi.com</a>
+        maintained by their users. More infos: <a href="https://www.omdbapi.com/" target="_blank" className="footer-link">www.omdbapi.com</a> <br />
+        <a href="https://de.vecteezy.com/gratis-vektor/kino" target="_blank" className="footer-link">Background image from Vecteezy</a>
       </p>
     </footer>
   );
